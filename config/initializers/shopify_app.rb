e1509fc79dcaba3183205637691954a32eb95ad5
paws <pas8314@gmail.com>
2016-03-22 18:16:12 -0400

    include store creds (for demo purposes)

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = "cd5eadb93a86c85973ab99d5eb27d61d"
+  config.secret = "ad2531dac265142c6177e9de8189de80"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
